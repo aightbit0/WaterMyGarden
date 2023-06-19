8c585f54b2b4f2013f524b8f46c82eaf1db3667c
aightbit0 <svenlatka@web.de>
2023-06-19 23:22:12 +0200

    new code

--- watermaygarden/watermaygarden.ino
+++ watermaygarden/watermaygarden.ino
@@ -1,138 +1,46 @@
-//pins
-const int WATERLEVEL_PIN = A3;
-const int RELAY_PIN = 3;
-const int WATERSENSOR_PIN1 = A0;
-const int WATERSENSOR_PIN2 = A1;
-//water options
-const int WATERAT = 570;
-const int WATERSTOPAT = 370;
-bool WATEREMPTY = false;
-
-int const TIMEDEAFAULT = 15000; // 15sec
-int const TIMEFAST = 1000; // 1sec
-
-int TIME = TIMEDEAFAULT;
-int const MINVALUE = 900;
-bool PUMPON = false;
-bool EMERGENCYSTOP = false;
-int const MAXPUMPTIMECOUNTER = 90; // 90 * 1sec = 90
-int const MAXWAITTOCONTINUEPUMPCOUNT = 480; // 60sec / 15sec = 4 * 120 = 480 (2std)
-int PUMPTIMECOUNTER = 0;
-int WAITTOCONTINUEPUMPCOUNT = 0;
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(6,INPUT); //input pin coming from soil sensor
+  pinMode(8,INPUT); //input pin coming from tank sensor
+  pinMode(7, OUTPUT); //pin for the relais
 }
 
-void loop()
-{
-  startNormalThread();
+long int scanTime = 0;
+long int maxPumpTime = 0;
+bool pumpActive = false;
+
+const int TANK_PIN = 8;
+const int SENSOR_PIN = 6;
+const int SECOND_SENSOR_PIN = 9;
+const int RELAIS_PIN = 7;
+
+void loop() {
+   //emergency stop
+    if (millis() >= scanTime) {
+      if(checkPinState(TANK_PIN)){
+         digitalWrite(RELAIS_PIN, LOW);
+         pumpActive = false;
+      }else if(checkPinState(SENSOR_PIN) || checkPinState(SECOND_SENSOR_PIN)){
+         digitalWrite(RELAIS_PIN, HIGH);
+         if(!pumpActive){
+          pumpActive = true;
+          //maxPumpTime = millis()+ 30000;
+         }
+      }else{
+        pumpActive = false;
+        digitalWrite(RELAIS_PIN, LOW);
+      }
+      if(pumpActive){
+        scanTime = millis()+ 2500;
+      }else{
+        scanTime = millis()+ 25000;
+      }
+    }
 }
 
-//reads value from sensor
-int getValue(int pin) {
-  int value = analogRead(pin);
-  Serial.println(value);
-  return value;
-}
-
-//function who set pump on or off
-void pumpOnOff(bool state) {
-  if (WATEREMPTY == true) {
-    Serial.println("Pump off WATER EMPTY !");
-    digitalWrite(RELAY_PIN, LOW);
-    PUMPON = false;
-    TIME = TIMEDEAFAULT;
-    return;
+bool checkPinState(int pin){
+  if(digitalRead(pin) == 1){
+    return true;
   }
-  if (WATEREMPTY == false) {
-    if (state == true) {
-      Serial.println("Pump on");
-      digitalWrite(RELAY_PIN, HIGH);
-      TIME = TIMEFAST;
-      PUMPON = true;
-    } else if (state == false) {
-      Serial.println("Pump off");
-      digitalWrite(RELAY_PIN, LOW);
-      PUMPON = false;
-      TIME = TIMEDEAFAULT;
-    }
-  }
-}
-
-//function who check if pump should be on or off
-void checkIfPUMPONOrOFF(int sensorValue1, int sensorValue2) {
-  if (EMERGENCYSTOP == false && PUMPON == true) {
-    PUMPTIMECOUNTER = PUMPTIMECOUNTER + 1;
-    if (PUMPTIMECOUNTER == MAXPUMPTIMECOUNTER) {
-      Serial.println("reached max pump time shutting off pump..");
-      PUMPTIMECOUNTER = 0;
-      EMERGENCYSTOP = true;
-      pumpOnOff(false);
-      return;
-    }
-  }
-  //set back PUMPTIMECOUNTER value to 0
-  if (EMERGENCYSTOP == false && PUMPON == false) {
-    if (PUMPTIMECOUNTER != 0) {
-      PUMPTIMECOUNTER = 0;
-    }
-  }
-  if (EMERGENCYSTOP == true) {
-    WAITTOCONTINUEPUMPCOUNT = WAITTOCONTINUEPUMPCOUNT + 1;
-    if (WAITTOCONTINUEPUMPCOUNT == MAXWAITTOCONTINUEPUMPCOUNT) {
-      Serial.println("reached deadline");
-      EMERGENCYSTOP = false;
-      WAITTOCONTINUEPUMPCOUNT = 0;
-    }
-    return;
-  }
-
-  Serial.print("Sensor value: ");
-  int value = getValue(WATERLEVEL_PIN);
-  if (value > MINVALUE) {
-    Serial.println("WATER IS EMPTY !");
-    if (WATEREMPTY == false) {
-      WATEREMPTY = true;
-      pumpOnOff(false);
-    }
-    return;
-  }
-  if (value <= MINVALUE) {
-    Serial.println("WATER OK");
-    WATEREMPTY = false;
-    if ((sensorValue1 >= WATERAT || sensorValue2 >= WATERAT) && PUMPON == false) {
-      pumpOnOff(true);
-    } else if ((sensorValue1 <= WATERSTOPAT && sensorValue2 <= WATERSTOPAT) && PUMPON == true) {
-      pumpOnOff(false);
-    }
-  }
-}
-
-void readValues() {
-  int oneValue = getValue(WATERSENSOR_PIN1);
-  int twoValue = getValue(WATERSENSOR_PIN2);
-  checkIfPUMPONOrOFF(oneValue, twoValue);
-}
-
-// function who waits given time but not block like delay()
-int startNormalThread() {
-  static unsigned long int waitSince = 0;
-  static bool state = true;
-
-  if (state == true) {
-    if (millis() - waitSince >= TIME) {
-      readValues();
-      state = false;
-      waitSince = millis();
-    }
-  } else if (state == false) {
-    if (millis() - waitSince >= TIME) {
-      readValues();
-      state = true;
-      waitSince = millis();
-    }
-  }
+  return false;
 }
